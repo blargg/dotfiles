365eeaa4e1d8b157c2101b749aa7d5c42b4b5b40
blargg <tomjankauski@gmail.com>
2020-08-30 16:12:49 -0700

    exclude private directories from history

--- .commonsh/variables.sh
+++ .commonsh/variables.sh
@@ -4,5 +4,6 @@ export BROWSER="chromium"
 export EDITOR="nvim"
 export PAGER="less"
 export PATH="$PATH:$HOME/.local/bin"
+export _ZO_EXCLUDE_DIRS="$HOME/private"
 
 eval $(dircolors -b ~/.dircolors)
