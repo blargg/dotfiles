c69e66969d8f87114c7ed3de8f8e3598eff1afff
blargg <tomjankauski@gmail.com>
2021-03-20 13:23:47 -0700

    add cargo install to path

--- .commonsh/variables.sh
+++ .commonsh/variables.sh
@@ -4,6 +4,8 @@ export BROWSER="chromium"
 export EDITOR="nvim"
 export PAGER="less"
 export PATH="$PATH:$HOME/.local/bin"
+# Cargo install location
+export PATH="$PATH:$HOME/.cargo/bin"
 export _ZO_EXCLUDE_DIRS="$HOME/private"
 
 eval $(dircolors -b ~/.dircolors)
